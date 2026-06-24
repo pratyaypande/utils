1a064bba9491a5c51d6ff1c6af547b909e0ca323
Pratyay Pande <pratyaypande@gmail.com>
2026-06-24 10:43:31 +0530

    Key-mapping to accept copilot code

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -2,6 +2,10 @@ return
 {
     {
         "github/copilot.vim",
+        config = function()
+			vim.g.copilot_no_tab_map = true
+			vim.keymap.set("i", "<C-j>", 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
+  		end,
         lazy = false,
         priority = 1000
     }
