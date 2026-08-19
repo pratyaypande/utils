a61d3d793936faa70f60c03114099af80d412865
Pratyay Pande <pratyaypande@gmail.com>
2026-08-19 11:26:25 +0530

    LSP Server for IR

--- dev-env-setup.sh
+++ dev-env-setup.sh
@@ -30,22 +30,22 @@ git checkout llvmorg-22.1.8 # use a release version: 21.0.0
 	
 echo -------- Configuring LLVM. Projects: clang and lld. This will take almost 5 minutes...
 # Use GNU Make as it will be there in 
-cmake -G 'Ninja' -B $PWD/build/Release -S $PWD/llvm    \
-	-DCMAKE_BUILD_TYPE=Release                                  \
-	-DCMAKE_C_COMPILER=$(which gcc)                             \
-	-DCMAKE_CXX_COMPILER=$(which g++)                           \
-	-DCMAKE_EXPORT_COMPILE_COMMANDS=ON                          \
-	-DCMAKE_INSTALL_PREFIX=$INSTALL_DIR/llvm                    \
-	-DLLVM_ENABLE_PROJECTS="clang;lld;clang-tools-extra"        \
-	-DLLVM_ENABLE_RUNTIMES="openmp"                             \
-	-DLLVM_ENABLE_ASSERTIONS=OFF                                \
-	-DLLVM_ENABLE_LTO=ON                                        \
-	-DLLVM_ENABLE_PIC=ON                                        \
-	-DBUILD_SHARED_LIBS=ON                                      \
-	-DLLVM_TARGETS_TO_BUILD="AMDGPU;X86"                        \
-	-DLLVM_PARALLEL_COMPILE_JOBS=$parallel_compile_job_num      \
-	-DLLVM_PARALLEL_LINK_JOBS=$parallel_link_jobs_num           \
-	-DLLVM_OPTIMIZED_TABLEGEN=TRUE                              \
+cmake -G 'Ninja' -B $PWD/build/Release -S $PWD/llvm                      \
+	-DCMAKE_BUILD_TYPE=Release                                           \
+	-DCMAKE_C_COMPILER=$(which gcc)                                      \
+	-DCMAKE_CXX_COMPILER=$(which g++)                                    \
+	-DCMAKE_EXPORT_COMPILE_COMMANDS=ON                                   \
+	-DCMAKE_INSTALL_PREFIX=$INSTALL_DIR/llvm                             \
+	-DLLVM_ENABLE_PROJECTS="clang;lld;clang-tools-extra;llvm-lsp-server" \
+	-DLLVM_ENABLE_RUNTIMES="openmp"                                      \
+	-DLLVM_ENABLE_ASSERTIONS=OFF                                         \
+	-DLLVM_ENABLE_LTO=ON                                                 \
+	-DLLVM_ENABLE_PIC=ON                                                 \
+	-DBUILD_SHARED_LIBS=ON                                               \
+	-DLLVM_TARGETS_TO_BUILD="AMDGPU;X86"                                 \
+	-DLLVM_PARALLEL_COMPILE_JOBS=$parallel_compile_job_num               \
+	-DLLVM_PARALLEL_LINK_JOBS=$parallel_link_jobs_num                    \
+	-DLLVM_OPTIMIZED_TABLEGEN=TRUE                                       \
 	2>&1 | tee $LOG_DIR/cmake-configure-llvm.log
 
 echo -------- Compiling LLVM: clang and lld. This will take almost 4 hours ...
